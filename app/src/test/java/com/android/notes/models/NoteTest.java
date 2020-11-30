d064cb1ccc1a2350bb58b09684a6dee2a7a95928
sp <saiprasadreddy081@gmail.com>
2020-11-30 17:41:16 -0500

    Dependencies, Modeling the data(Note) and Test writing process

---
+++ app/src/test/java/com/android/notes/models/NoteTest.java
@@ -0,0 +1,63 @@
+package com.android.notes.models;
+
+import org.junit.jupiter.api.Test;
+
+public class NoteTest {
+    /*
+    * Compare two equal notes...
+    */
+
+    @Test
+    void isNotesEqual_identicalProperties() throws Exception {
+
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+
+
+    /*
+    * Compare notes with 2 different ids
+    */
+
+    /*
+    * Compare notes with different timestamps
+     */
+
+    /*
+    * Compare two notes with different titles
+    */
+
+    /*
+    * compare two notes with different content
+    */
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
